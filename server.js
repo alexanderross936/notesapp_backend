eadefd58e99047b1a9282ed4398ea24e9c200a42
Alexander Ross <alexanderross936@gmail.com>
2020-10-02 14:34:48 -0500

    added json

--- server.js
+++ server.js
@@ -20,10 +20,9 @@ connectDB()
 const app = express();
 
 
-app.use(express.json());
-// app.use(express.urlencoded());
 app.use(cors());
-// app.use(express.static())
+app.use(express.json());
+app.use(express.urlencoded());
 
 
 app.get('/api/user', auth, async(req, res) => {
