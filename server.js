b14149a77b76fb8d81763329fd6df053a3f83d6c
Alexander Ross <alexanderross936@gmail.com>
2020-10-03 21:51:06 -0500

    added json

--- server.js
+++ server.js
@@ -17,12 +17,13 @@ connectDB()
 
 // var db = mongoose.connection;
 
-const app = express();
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
 
+app.use(bodyParser.json());
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/api/user', auth, async(req, res) => {
