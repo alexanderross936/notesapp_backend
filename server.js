429e481da5fe26acf05a0c8b71aeb80d830fa3f1
Alexander Ross <alexanderross936@gmail.com>
2021-04-15 14:22:28 -0500

    change server.js

--- server.js
+++ server.js
@@ -228,9 +228,6 @@ app.delete('/api/note/delete/:id', (req, res) => {
     });
 })
 
-
-if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
-
 app.listen(process.env.PORT || 4000, () => {
     console.log('App listening on PORT 4000')
 })
